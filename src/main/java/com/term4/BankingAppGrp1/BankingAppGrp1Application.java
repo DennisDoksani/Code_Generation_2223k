fa7f5dcd9a643dba7c29a4666433d899e5af1012
bjsapkota1313 <90284672+bjsapkota1313@users.noreply.github.com>
2023-05-11 21:20:10 +0200

    Java Base

---
+++ src/main/java/com/term4/BankingAppGrp1/BankingAppGrp1Application.java
@@ -0,0 +1,13 @@
+package com.term4.BankingAppGrp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingAppGrp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingAppGrp1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/term4/BankingAppGrp1/BankingAppGrp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.term4.BankingAppGrp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingAppGrp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
