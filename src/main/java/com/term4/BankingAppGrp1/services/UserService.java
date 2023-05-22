d033e729553e8888085b3cc9533ceb42e62c41f9
Joshua Andrea <90309312+Josh684077@users.noreply.github.com>
2023-05-22 21:10:29 +0200

    added user details service

--- src/main/java/com/term4/BankingAppGrp1/services/UserService.java
+++ src/main/java/com/term4/BankingAppGrp1/services/UserService.java
@@ -2,7 +2,20 @@ package com.term4.BankingAppGrp1.services;
 
 import org.springframework.stereotype.Service;
 
+import com.term4.BankingAppGrp1.models.User;
+import com.term4.BankingAppGrp1.repositories.UserRepository;
+
+
 @Service
 public class UserService {
+    
+    private final UserRepository userRepository;
 
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User saveUser(User user){
+        return userRepository.save(user);
+    }
 }
