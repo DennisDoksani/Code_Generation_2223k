9af6594a41a7701f236d190a6c24a2addbc16099
Joshua Andrea <90309312+Josh684077@users.noreply.github.com>
2023-05-22 18:45:21 +0200

    created jwt utilities

---
+++ src/main/java/com/term4/BankingAppGrp1/configuration/WebSecurityConfiguration.java
@@ -0,0 +1,10 @@
+package com.term4.BankingAppGrp1.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfiguration {
+    
+}
