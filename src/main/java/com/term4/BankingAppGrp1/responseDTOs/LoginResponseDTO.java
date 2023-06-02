f6eb95e4755c669779dec219400c3e4753c4f98a
Joshua Andrea <90309312+Josh684077@users.noreply.github.com>
2023-06-02 08:38:01 +0200

    edited login response token

--- src/main/java/com/term4/BankingAppGrp1/responseDTOs/LoginResponseDTO.java
+++ src/main/java/com/term4/BankingAppGrp1/responseDTOs/LoginResponseDTO.java
@@ -1,5 +1,5 @@
 package com.term4.BankingAppGrp1.responseDTOs;
 
-public record LoginResponseDTO(String token, long id, String email, String name) {
+public record LoginResponseDTO(String jwt, long id, String email, String name) {
     
 }
