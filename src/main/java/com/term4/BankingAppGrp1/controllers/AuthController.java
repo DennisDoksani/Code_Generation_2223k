ec3825ff65c9686b853682e95aa6cad58fa412af
bjsapkota1313 <681990@student.inholland.nl>
2023-06-09 17:20:02 +0200

    FIxed Not null Import

--- src/main/java/com/term4/BankingAppGrp1/controllers/AuthController.java
+++ src/main/java/com/term4/BankingAppGrp1/controllers/AuthController.java
@@ -3,8 +3,7 @@ package com.term4.BankingAppGrp1.controllers;
 import javax.naming.AuthenticationException;
 
 import jakarta.validation.Valid;
-import org.jetbrains.annotations.NotNull;
-import org.springframework.security.access.prepost.PreAuthorize;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
