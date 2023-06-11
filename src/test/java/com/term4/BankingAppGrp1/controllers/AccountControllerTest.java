d6af713e2370d24058ab031c82cb8a2fc6b1de2c
bjsapkota1313 <681990@student.inholland.nl>
2023-06-11 09:29:34 +0200

    Acoount Controller Tets

--- src/test/java/com/term4/BankingAppGrp1/controllers/AccountControllerTest.java
+++ src/test/java/com/term4/BankingAppGrp1/controllers/AccountControllerTest.java
@@ -15,20 +15,14 @@ import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -46,8 +40,6 @@ class AccountControllerTest extends BankingAppTestData {
   private AccountService accountService;
   @MockBean
   private UserService userService;
-  
-
 
   @BeforeEach
   void init() {
@@ -176,6 +168,7 @@ class AccountControllerTest extends BankingAppTestData {
         .andExpect(status().isForbidden())
         .andExpect(jsonPath("$.message").value("You are not allowed to access this account!"));
   }
+
   @Test
   @WithMockUser(roles = {"CUSTOMER"}, username = CUSTOMER_EMAIL)
   void whenCustomerAuthIsProvidedToGETOneAccountWhichJWTUserIsOwnerOfItReturnsOK()
@@ -205,6 +198,7 @@ class AccountControllerTest extends BankingAppTestData {
             jsonPath("$.accountHolder.dayLimit").value(employeeAccount.getCustomer().getDayLimit()))
         .andExpect(jsonPath("$.active").value(employeeAccount.isActive()));
   }
+
   @Test
   @WithMockUser(roles = {"CUSTOMER"}, username = CUSTOMER_EMAIL)
   void whenCustomerOrEmployeeHaveValidJWTAndTryToAccessGETOneWithInvalidIBanPatternReturnsBadRequest()
@@ -220,106 +214,193 @@ class AccountControllerTest extends BankingAppTestData {
         .andExpect(status().isBadRequest())
         .andExpect(jsonPath("$.message").value("Not a valid Iban for Inholland bank"));
   }
+
   @Test
   @WithMockUser(roles = {"CUSTOMER"}, username = CUSTOMER_EMAIL)
   void whenCustomerHaveValidJWTAndTryToAccessPOSTAccountStatusWillGetForbidden() throws Exception {
     when(accountService.getAccountByIBAN(employeeAccount.getIban())).thenReturn(employeeAccount);
     this.mockMvc.perform(
-            MockMvcRequestBuilders.post("/accounts/" + "accountStatus/"+employeeAccount.getIban() ))
+            MockMvcRequestBuilders.post("/accounts/" + "accountStatus/" + employeeAccount.getIban()))
         .andDo(print())
         .andExpect(status().isForbidden());
 
   }
+
   @Test //TODO check this it is giving 403 instead of 204
   @WithMockUser(roles = {"EMPLOYEE"})
   void whenEmployeeHaveValidJWTAndTryToAccessPOSTAccountStatusWillGetNoContent() throws Exception {
     when(accountService.getAccountByIBAN(employeeAccount.getIban())).thenReturn(employeeAccount);
     AccountStatusDTO accountStatusDTO = new AccountStatusDTO(true);
     this.mockMvc.perform(
-            MockMvcRequestBuilders.post("/accounts/accountStatus/" +employeeAccount.getIban() )
+            MockMvcRequestBuilders.post("/accounts/accountStatus/" + employeeAccount.getIban())
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(accountStatusDTO)))
         .andDo(print())
         .andExpect(status().isNoContent());
 
   }
+
   @Test
   @WithMockUser(roles = {"EMPLOYEE"})
-  void whenEmployeeHaveValidJWTAndTryToAccessPOSTAccountStatusWithInvalidIBANWillGetBadRequest() throws Exception {
+  void whenEmployeeHaveValidJWTAndTryToAccessPOSTAccountStatusWithInvalidIBANWillGetBadRequest()
+      throws Exception {
     when(accountService.getAccountByIBAN(employeeAccount.getIban())).thenReturn(employeeAccount);
     AccountStatusDTO accountStatusDTO = new AccountStatusDTO(true);
     this.mockMvc.perform(
-            MockMvcRequestBuilders.post("/accounts/accountStatus/" + "invalidIBAN" )
+            MockMvcRequestBuilders.post("/accounts/accountStatus/" + "invalidIBAN")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(accountStatusDTO)))
         .andDo(print())
         .andExpect(status().isBadRequest());
 
   }
+
   @Test
-  @WithMockUser(roles = {"EMPLOYEE"})
-  void whenEmployeeHaveValidJWTAndTryToAccessPOSTAccountStatusWithNonExistingAccountReturnsNotFound() throws Exception {
-    when(accountService.getAccountByIBAN(employeeAccount.getIban())).thenThrow(new EntityNotFoundException("Account not found"));
+  @WithMockUser(roles = {"EMPLOYEE"}, username = EMPLOYEE_EMAIL)
+  void whenEmployeeHaveValidJWTAndTryToAccessPOSTAccountStatusWithNonExistingAccountReturnsNotFound()
+      throws Exception {
+    when(accountService.getAccountByIBAN(employeeAccount.getIban())).
+        thenThrow(new EntityNotFoundException("Account not found"));
     this.mockMvc.perform(
-            MockMvcRequestBuilders.post("/accounts/accountStatus/" + employeeAccount.getIban() )
+            MockMvcRequestBuilders.post("/accounts/accountStatus/" + employeeAccount.getIban())
         )
         .andDo(print())
         .andExpect(status().isNotFound())
         .andExpect(jsonPath("$.message").value("Account not found"));
   }
+
   @Test
   @WithMockUser(roles = {"EMPLOYEE"})
-  void whenEmployeeHaveValidJWTAndTryToAccessGETAccountsByEmailWithInvalidEmailReturnsBadRequest() throws Exception {
-    when(accountService.getAccountsByEmailAddress(employeeAccount.getCustomer().getEmail())).thenReturn(List.of(employeeAccount));
+  void whenEmployeeHaveValidJWTAndTryToAccessGETAccountsByEmailWithInvalidEmailReturnsBadRequest()
+      throws Exception {
+    when(accountService.getAccountsByEmailAddress(
+        employeeAccount.getCustomer().getEmail())).thenReturn(List.of(employeeAccount));
     this.mockMvc.perform(
-            MockMvcRequestBuilders.get("/accounts/user/" + "invalidEmail" )
+            MockMvcRequestBuilders.get("/accounts/user/" + "invalidEmail")
         )
         .andDo(print())
         .andExpect(status().isBadRequest())
         .andExpect(jsonPath("$.message").value("The email provided is not a valid email address"));
   }
+
   @Test
-  @WithMockUser(roles={"CUSTOMER"}, username = CUSTOMER_EMAIL)
-  void whenCustomerHaveValidJWTAndTryToAccessGETAccountsByEmailWhichIsDifferentEmailThanJWTEmailReturnsForbidden() throws Exception {
-    when(accountService.getAccountsByEmailAddress(employeeAccount.getCustomer().getEmail())).thenReturn(List.of(employeeAccount));
-    when(accountService.isAccountOwnedByCustomer(employeeAccount.getIban(), customerUser.getEmail())).thenReturn(false);
+  @WithMockUser(roles = {"CUSTOMER"})
+  void whenCustomerHaveValidJWTAndTryToAccessGETAccountsByEmailWhichIsDifferentEmailThanJWTEmailReturnsForbidden()
+      throws Exception {
+    when(accountService.getAccountsByEmailAddress(
+        employeeAccount.getCustomer().getEmail())).thenReturn(List.of(employeeAccount));
+    when(accountService.isAccountOwnedByCustomer(employeeAccount.getIban(),
+        customerUser.getEmail())).thenReturn(false);
     this.mockMvc.perform(
             MockMvcRequestBuilders.get("/accounts/user/" + employeeAccount.getCustomer().getEmail()))
         .andDo(print())
         .andExpect(status().isForbidden())
-        .andExpect(jsonPath("$.message").value("You are not allowed to access others Accounts Details!"));
+        .andExpect(
+            jsonPath("$.message").value("You are not allowed to access others Accounts Details!"));
   }
+
   @Test
-  @WithMockUser(roles={"CUSTOMER"}, username = CUSTOMER_EMAIL)
-  @WithUserDetails(CUSTOMER_EMAIL)
+  @WithMockUser(roles = {"CUSTOMER"}, username = CUSTOMER_EMAIL)
   void whenCustomerHaveAValidJWTAndTryToAccessGETAccountsByEmailSameWithJWTEmailReturnsOkWithAccount()
       throws Exception {
-    when(accountService.getAccountsByEmailAddress(CUSTOMER_EMAIL)).thenReturn(List.of(employeeAccount));
-    when(accountService.isAccountOwnedByCustomer(employeeAccount.getIban(), CUSTOMER_EMAIL)).thenReturn(true);
+    when(accountService.getAccountsByEmailAddress(CUSTOMER_EMAIL)).thenReturn(
+        List.of(employeeAccount));
+    when(accountService.isAccountOwnedByCustomer(employeeAccount.getIban(),
+        CUSTOMER_EMAIL)).thenReturn(true);
     when(userService.getUserByEmail(CUSTOMER_EMAIL)).thenReturn(employeeUser);
+    when(accountService.getTotalTransactedAmountOfTodayByUserEmail(
+        employeeUser.getEmail())).thenReturn(50.00);
+
     this.mockMvc.perform(
-            MockMvcRequestBuilders.get("/accounts/user/" +CUSTOMER_EMAIL))
+            MockMvcRequestBuilders.get("/accounts/user/" + CUSTOMER_EMAIL))
         .andDo(print())
         .andExpect(status().isOk())
-        .andExpect(jsonPath("$[0].iban").value(employeeAccount.getIban()))
-        .andExpect(jsonPath("$[0].accountBalance").value(employeeAccount.getBalance()))
-        .andExpect(jsonPath("$[0].accountType").value(employeeAccount.getAccountType().name()))
-        .andExpect(jsonPath("$[0].absoluteLimit").value(employeeAccount.getAbsoluteLimit()))
-        .andExpect(jsonPath("$[0].creationDate").value(employeeAccount.getCreationDate().toString()))
-        .andExpect(jsonPath("$[0].accountHolder.userId").value(employeeAccount.getCustomer().getId()))
-        .andExpect(jsonPath("$[0].accountHolder.firstName").value(
-            employeeAccount.getCustomer().getFirstName()))
-        .andExpect(
-            jsonPath("$[0].accountHolder.lastName").value(employeeAccount.getCustomer().getLastName()))
-        .andExpect(jsonPath("$[0].accountHolder.transactionLimit").value(
-            employeeAccount.getCustomer().getTransactionLimit()))
-        .andExpect(
-            jsonPath("$[0].accountHolder.dayLimit").value(employeeAccount.getCustomer().getDayLimit()))
-        .andExpect(jsonPath("$[0].active").value(employeeAccount.isActive()));
-
+        .andExpectAll(
+            jsonPath("$.accounts[0].iban").value(employeeAccount.getIban()),
+            jsonPath("$.accounts[0].accountBalance").value(employeeAccount.getBalance()),
+            jsonPath("$.accounts[0].accountType").value(employeeAccount.getAccountType().name()),
+            jsonPath("$.accounts[0].absoluteLimit").value(employeeAccount.getAbsoluteLimit()),
+            jsonPath("$.accounts[0].creationDate").value(
+                employeeAccount.getCreationDate().toString()),
+            jsonPath("$.accountHolder.userId").value(employeeAccount.getCustomer().getId()),
+            jsonPath("$.accountHolder.firstName").value(
+                employeeAccount.getCustomer().getFirstName()),
+            jsonPath("$.accountHolder.lastName").value(employeeAccount.getCustomer().getLastName()),
+            jsonPath("$.accountHolder.transactionLimit").value(
+                employeeAccount.getCustomer().getTransactionLimit()),
+            jsonPath("$.accountHolder.dayLimit").value(employeeAccount.getCustomer().getDayLimit()),
+            jsonPath("$.totalTransactedAmountToday").value(50.00),
+            jsonPath("$.totalBalance").value(1000.0)
+        );
   }
 
+  @Test
+  @WithAnonymousUser
+  void whenAnonymousUserTryToAccessGETAccountsByEmailReturnsUnauthorized() throws Exception {
+    when(accountService.getAccountsByEmailAddress(
+        employeeAccount.getCustomer().getEmail())).thenReturn(List.of(employeeAccount));
+    this.mockMvc.perform(
+            MockMvcRequestBuilders.get("/accounts/user/" + employeeAccount.getCustomer().getEmail()))
+        .andDo(print())
+        .andExpect(status().isUnauthorized());
+  }
 
+  @Test
+  @WithMockUser(roles = {"EMPLOYEE", "CUSTOMER"})
+  void whenUserHaveValidJWTAndTryToAccessGETSearchingAccountsByCustomerNameReturnsList()
+      throws Exception {
+    when(accountService.searchAccountByCustomerName(employeeUser.getFirstName(), 2, 0))
+        .thenReturn(List.of(employeeAccount, customerAccount));
+    this.mockMvc.perform(
+            MockMvcRequestBuilders.get("/accounts/searchByCustomerName")
+                .param("limit", "2")
+                .param("offset", "0")
+                .param("customerName", employeeUser.getFirstName()))
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.size()").value(2))
+        .andExpectAll(
+            jsonPath("$[0].accountHolder").value(employeeAccount.getCustomer().getFirstName()
+                + " " + employeeAccount.getCustomer().getLastName()),
+            jsonPath("$[0].iban").value(employeeAccount.getIban()),
+            jsonPath("$[1].accountHolder").value(customerAccount.getCustomer().getFirstName()
+                + " " + customerAccount.getCustomer().getLastName()),
+            jsonPath("$[1].iban").value(customerAccount.getIban())
+        );
+  }
+
+  @Test
+  @WithMockUser(roles = {"EMPLOYEE", "CUSTOMER"})
+  void whenUserHaveValidJWTAndTryToAccessGETSearchingAccountsByCustomerNameReturnsNotFound()
+      throws Exception {
+    String searchingName = employeeUser.getFirstName();
+    when(accountService.searchAccountByCustomerName(searchingName, 2, 0))
+        .thenReturn(List.of());
+    this.mockMvc.perform(
+            MockMvcRequestBuilders.get("/accounts/searchByCustomerName")
+                .param("limit", "2")
+                .param("offset", "0")
+                .param("customerName", searchingName))
+        .andDo(print())
+        .andExpect(status().isNotFound())
+        .andExpect(
+            jsonPath("$.message").value("No accounts found by this name " + searchingName + "!"));
+  }
+
+  @Test
+  @WithMockUser(roles = {"EMPLOYEE", "CUSTOMER"})
+  void whenUserHaveValidJWTAndTryToAccessGETSearchingAccountsByBlankCustomerNameReturnsBadRequest()
+      throws Exception {
+    String searchingName = "";
+    this.mockMvc.perform(
+            MockMvcRequestBuilders.get("/accounts/searchByCustomerName")
+                .param("limit", "0")
+                .param("offset", "0")
+                .param("customerName", searchingName))
+        .andDo(print())
+        .andExpect(status().isBadRequest())
+        .andExpect(jsonPath("$.message").value("Customer name cannot be empty in order to search"));
+  }
 
 
 }
--- src/test/java/com/term4/BankingAppGrp1/testingData/BankingAppTestData.java
+++ src/test/java/com/term4/BankingAppGrp1/testingData/BankingAppTestData.java
@@ -11,22 +11,25 @@ import java.time.LocalDate;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 
-public abstract class BankingAppTestData  {
+public abstract class BankingAppTestData {
+
   protected ObjectMapper objectMapper;
   protected Account employeeAccount;
-  protected final String CUSTOMER_EMAIL="customer@seed.com";
-  protected final String EMPLOYEE_EMAIL="employee@seed.com";
-  protected final String EMPLOYEE_CUSTOMER_EMAIL="employeeCustomer@seed.com";
+  protected Account customerAccount;
+  protected final String CUSTOMER_EMAIL = "customer@seed.com";
+  protected final String EMPLOYEE_EMAIL = "employee@seed.com";
+  protected final String EMPLOYEE_CUSTOMER_EMAIL = "employeeCustomer@seed.com";
   protected User employeeUser;
-  protected  User customerUser;
+  protected User customerUser;
 
   protected AccountDTO accountDTO;
 
   protected AccountHolderDTO accountHolderDTO;
+
   @BeforeEach
-  protected void setupData(){
-    objectMapper=new ObjectMapper();
-    customerUser= User.builder()
+  protected void setupData() {
+    objectMapper = new ObjectMapper();
+    customerUser = User.builder()
         .id(3L)
         .bsn("091287662")
         .firstName("Employee")
@@ -40,7 +43,7 @@ public abstract class BankingAppTestData  {
         .isActive(true)
         .roles(List.of(Role.ROLE_CUSTOMER))
         .build();
-    employeeUser= User.builder()
+    employeeUser = User.builder()
         .id(1)
         .bsn("277545146")
         .firstName("EmployeeCustomer")
@@ -62,12 +65,21 @@ public abstract class BankingAppTestData  {
         .isActive(true)
         .customer(employeeUser)
         .build();
-    accountHolderDTO= new AccountHolderDTO(employeeUser.getId(),employeeUser.getDayLimit(),employeeUser.getTransactionLimit(),
-        employeeUser.getFirstName(),employeeUser.getLastName());
-    accountDTO= new AccountDTO(employeeAccount.getIban(),employeeAccount.getBalance(),
-        employeeAccount.getAbsoluteLimit(),employeeAccount.getCreationDate()
-        ,employeeAccount.isActive(),
-        employeeAccount.getAccountType(),accountHolderDTO);
+    customerAccount = Account.builder()
+        .iban("NL72INHO0579629980")
+        .accountType(AccountType.CURRENT)
+        .creationDate(LocalDate.now())
+        .balance(500)
+        .isActive(true)
+        .customer(customerUser)
+        .build();
+    accountHolderDTO = new AccountHolderDTO(employeeUser.getId(), employeeUser.getDayLimit(),
+        employeeUser.getTransactionLimit(),
+        employeeUser.getFirstName(), employeeUser.getLastName());
+    accountDTO = new AccountDTO(employeeAccount.getIban(), employeeAccount.getBalance(),
+        employeeAccount.getAbsoluteLimit(), employeeAccount.getCreationDate()
+        , employeeAccount.isActive(),
+        employeeAccount.getAccountType(), accountHolderDTO);
   }
 
 }
