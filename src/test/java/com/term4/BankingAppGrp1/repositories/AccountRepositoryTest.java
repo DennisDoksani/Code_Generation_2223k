0c51280b589aec93650187347cc4ca5b628db42a
bjsapkota1313 <681990@student.inholland.nl>
2023-06-13 18:04:47 +0200

    Repository Test with DataJpa

--- src/test/java/com/term4/BankingAppGrp1/repositories/AccountRepositoryTest.java
+++ src/test/java/com/term4/BankingAppGrp1/repositories/AccountRepositoryTest.java
@@ -1,264 +1,228 @@
 package com.term4.BankingAppGrp1.repositories;
 
+import static com.term4.BankingAppGrp1.models.ConstantsContainer.DEFAULT_INHOLLAND_BANK_IBAN;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.term4.BankingAppGrp1.generators.IBANGenerator;
 import com.term4.BankingAppGrp1.models.Account;
 import com.term4.BankingAppGrp1.models.AccountType;
-import com.term4.BankingAppGrp1.models.Role;
-import com.term4.BankingAppGrp1.models.User;
-import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
-import java.util.function.Function;
-import org.springframework.data.domain.Example;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.jpa.domain.Specification;
-import org.springframework.data.repository.query.FluentQuery;
+import org.springframework.data.domain.PageRequest;
 
 
-class AccountRepositoryTest {
+class AccountRepositoryTest extends BaseRepositoryTest {
 
-  private AccountRepository accountRepository = new AccountRepository() {
+  @Autowired
+  private AccountRepository accountRepository;
+  @MockBean
+  private IBANGenerator ibanGenerator; // used for generating iban
 
-    User user = User.builder()
-        .bsn("277545146")
-        .firstName("EmployeeCustomer")
-        .lastName("Seed")
-        .dateOfBirth(LocalDate.of(1990, 1, 1))
-        .phoneNumber("0611111111")
-        .email("employeecustomer@seed.com")
-        .password("password")
-        .isActive(true)
-        .roles(List.of(Role.ROLE_EMPLOYEE, Role.ROLE_CUSTOMER))
-        .dayLimit(1000)
-        .transactionLimit(300)
-        .build();
-    Account testingAccount = Account.builder()
-        .iban("NL72INHO0579629781")
-        .balance(900.0)
-        .creationDate(LocalDate.now())
-        .accountType(AccountType.CURRENT)
-        .customer(user)
-        .build();
 
-    @Override
-    public List<Account> findAll(Sort sort) {
-      return null;
-    }
+  @BeforeEach
+  void setUp() {
+    super.setupData();
+  }
 
-    @Override
-    public Page<Account> findAll(Pageable pageable) {
-      return null;
-    }
+  @Test
+  void savingAccountShouldReturnSavedAccount() {
+    userRepository.save(employeeUser);
+    Account savedAccount = accountRepository.save(employeeAccount);
+    assertNotNull(savedAccount);
+    assertEquals(employeeAccount.getBalance(), savedAccount.getBalance());
+    assertEquals(employeeAccount.getAccountType(), savedAccount.getAccountType());
+    assertEquals(employeeAccount.getCustomer(), savedAccount.getCustomer());
+    assertEquals(employeeAccount.getIban(), savedAccount.getIban());
+  }
 
-    @Override
-    public <S extends Account> S save(S entity) {
-      return null;
-    }
+  @Test
+  void savingAccountShouldGenerateIban() {
+    userRepository.save(employeeUser);
+    Account newAccount = Account.builder().accountType(AccountType.CURRENT)
+        .customer(employeeUser).balance(1000.0).build();
+    Account savedAccount = accountRepository.save(newAccount);
+    assertNotNull(savedAccount);
+    assertNotNull(savedAccount.getIban());
+  }
 
-    @Override
-    public <S extends Account> List<S> saveAll(Iterable<S> entities) {
-      return null;
-    }
+  @Test
+  void gettingAllAccountsShouldReturnAllAccount() {
+    userRepository.save(employeeUser);
+    accountRepository.save(employeeAccount);
+    accountRepository.save(Account.builder().accountType(AccountType.CURRENT)
+        .customer(employeeUser).balance(1000.0).build());
 
-    @Override
-    public Optional<Account> findById(String s) {
-      return Optional.empty();
-    }
+    List<Account> allAccounts = accountRepository.findAll();
+    assertNotNull(allAccounts);
+    assertEquals(2, allAccounts.size());
 
-    @Override
-    public boolean existsById(String s) {
-      return false;
-    }
+  }
 
-    @Override
-    public List<Account> findAll() {
-      return null;
-    }
+  @Test
+  void gettingAccountIdShouldReturnAccountObject() {
+    userRepository.save(employeeUser);
+    accountRepository.save(employeeAccount);
+    Optional<Account> foundAccount = accountRepository.findById(employeeAccount.getIban());
+    assertNotNull(foundAccount);
+  }
 
-    @Override
-    public List<Account> findAllById(Iterable<String> strings) {
-      return null;
-    }
+  @Test
+  void gettingAccountIdShouldReturnEmptyOptional() {
+    Optional<Account> foundAccount = accountRepository.findById(employeeAccount.getIban());
+    assertEquals(Optional.empty(), foundAccount);
+  }
 
-    @Override
-    public long count() {
-      return 0;
-    }
+  @Test
+  void findByAndIbanNotShouldNotReturnAccountWhereIbanIs() {
+    userRepository.save(employeeUser);
+    userRepository.save(inhollandBankUser);
+    accountRepository.save(employeeAccount);
+    accountRepository.save(inhollandBankAccount);
+    Page<Account> pageAccounts = accountRepository.findByAndIbanNot(PageRequest.of(0, 3),
+        DEFAULT_INHOLLAND_BANK_IBAN);
+    List<Account> foundAccounts = pageAccounts.getContent();
+    assertEquals(1, foundAccounts.size());
+    assertEquals(employeeAccount, foundAccounts.get(0));
+    assertNotEquals(inhollandBankAccount, foundAccounts.get(0));
+  }
 
-    @Override
-    public void deleteById(String s) {
+  @Test
+  void findAccountByAccountTypeEqualsAndIbanNotShouldNotReturnAccountWhereIbanIsAndTypeOfAccountSpecified() {
+    userRepository.save(employeeUser);
+    userRepository.save(inhollandBankUser);
+    accountRepository.save(employeeAccount);
+    accountRepository.save(inhollandBankAccount);
+    accountRepository.save(Account.builder().accountType(AccountType.SAVINGS)
+        .customer(employeeUser).balance(1000.0).build());
+    Page<Account> pageAccounts = accountRepository.findAccountByAccountTypeEqualsAndIbanNot(
+        PageRequest.of(0, 3),
+        AccountType.SAVINGS, DEFAULT_INHOLLAND_BANK_IBAN);
+    List<Account> foundAccounts = pageAccounts.getContent();
+    assertEquals(1, foundAccounts.size());
+    assertNotEquals(employeeAccount, foundAccounts.get(0));
+    assertNotEquals(inhollandBankAccount, foundAccounts.get(0));
+    assertEquals(AccountType.SAVINGS, foundAccounts.get(0).getAccountType());
+  }
 
-    }
+  @Test
+  void findByCustomer_IDEqualsShouldReturnAllAccountsOfCustomer() {
+    userRepository.save(employeeUser);
+    userRepository.save(inhollandBankUser);
+    accountRepository.save(employeeAccount);
+    accountRepository.save(inhollandBankAccount);
+    accountRepository.save(Account.builder().accountType(AccountType.SAVINGS)
+        .customer(employeeUser).balance(1000.0).build());
+    List<Account> foundAccounts = accountRepository.findByCustomer_IdEquals(employeeUser.getId());
+    assertEquals(2, foundAccounts.size());
+    assertEquals(employeeAccount, foundAccounts.get(0));
+    assertNotEquals(inhollandBankAccount, foundAccounts.get(0));
+    assertEquals(employeeUser.getId(), foundAccounts.get(0).getCustomer().getId());
+    assertEquals(employeeUser.getId(), foundAccounts.get(1).getCustomer().getId());
+  }
 
-    @Override
-    public void delete(Account entity) {
+  @Test
+  void countAccountByCustomer_IdEqualsAndAccountTypeEqualsShouldReturnNumberOfAccountOwned() {
+    userRepository.save(employeeUser);
+    accountRepository.save(employeeAccount);
+    userRepository.save(inhollandBankUser);
+    accountRepository.save(inhollandBankAccount);
+    accountRepository.save(Account.builder().accountType(AccountType.CURRENT)
+        .customer(employeeUser).balance(1000.0).build());
+    int numberOfAccounts = accountRepository.countAccountByCustomer_IdEqualsAndAccountTypeEquals(
+        employeeUser.getId(), AccountType.CURRENT);
+    assertEquals(2, numberOfAccounts);
 
-    }
+  }
 
-    @Override
-    public void deleteAllById(Iterable<? extends String> strings) {
+  @Test
+  void existAccountByIbanEqualsAndCustomerEmailEqualsIgnoreCaseShouldReturnTrueWhenTheyHaveAccount() {
+    userRepository.save(employeeUser);
+    accountRepository.save(employeeAccount);
+    boolean accountExists = accountRepository.existsAccountByIbanEqualsAndCustomerEmailEqualsIgnoreCase(
+        employeeAccount.getIban(), employeeUser.getEmail());
+    assertTrue(accountExists);
+  }
 
-    }
+  @Test
+  void existAccountByIbanEqualsAndCustomerEmailEqualsIgnoreCaseShouldReturnFalseWhenTheyDontHaveAccount() {
+    userRepository.save(employeeUser);
+    accountRepository.save(employeeAccount);
+    boolean accountExists = accountRepository.existsAccountByIbanEqualsAndCustomerEmailEqualsIgnoreCase(
+        employeeAccount.getIban(), inhollandBankUser.getEmail());
+    assertFalse(accountExists);
+  }
 
-    @Override
-    public void deleteAll(Iterable<? extends Account> entities) {
+  @Test
+  void findAccountsWithIsNotBankAccountSpecificationShouldReturnBankAccount() {
+    userRepository.save(employeeUser);
+    userRepository.save(inhollandBankUser);
+    accountRepository.save(employeeAccount);
+    accountRepository.save(inhollandBankAccount);
+    accountRepository.save(Account.builder().accountType(AccountType.SAVINGS)
+        .customer(employeeUser).balance(1000.0).build());
+    List<Account> foundAccounts = accountRepository.findAll(
+        AccountSpecifications.isNotBanksOwnAccount());
+    assertEquals(2, foundAccounts.size());
+    assertFalse(foundAccounts.contains(inhollandBankAccount));
+    assertTrue(foundAccounts.contains(employeeAccount));
+  }
 
-    }
+  @Test
+  void findAccountsWithIsActiveSpecificationReturnsActiveAccountOnly() {
+    userRepository.save(employeeUser);
+    accountRepository.save(employeeAccount);
+    Account deactivatedAccount = Account.builder().accountType(AccountType.SAVINGS)
+        .customer(employeeUser).balance(1000.0).isActive(false).
+        build();
+    accountRepository.save(deactivatedAccount);
+    List<Account> foundAccounts = accountRepository.findAll(
+        AccountSpecifications.isActiveAccounts());
+    assertEquals(1, foundAccounts.size());
+    assertFalse(foundAccounts.contains(deactivatedAccount));
+  }
 
-    @Override
-    public void deleteAll() {
+  @Test
+  void findAllWithCustomerNameSpecificationReturnsMatchingNameAccounts() {
+    userRepository.save(employeeUser);
+    accountRepository.save(employeeAccount);
+    List<Account> foundAccounts = accountRepository.findAll(
+        AccountSpecifications.hasCustomerName(employeeUser.getFirstName()));
+    assertEquals(1, foundAccounts.size());
+    assertTrue(foundAccounts.contains(employeeAccount));
+  }
 
-    }
+  @Test
+  void findAllWithHasCustomerEmailSpecificationShouldReturnAllAccountsSpecifiedEmailAndShouldBeCaseInsensitive() {
+    userRepository.save(employeeUser);
+    accountRepository.save(employeeAccount);
+    List<Account> foundAccounts = accountRepository.findAll(
+        AccountSpecifications.hasCustomerEmail(employeeUser.getEmail().toLowerCase()));
+    assertEquals(1, foundAccounts.size());
+    assertTrue(foundAccounts.contains(employeeAccount));
+  }
 
-    @Override
-    public Optional<Account> findOne(Specification<Account> spec) {
-      return Optional.empty();
-    }
+  @Test
+  void findAllAccountsWhichHasIsCurrentAccountSpecificationShouldReturnAllCurrentAccounts() {
+    userRepository.save(employeeUser);
+    accountRepository.save(employeeAccount);
+    Account savingsAccount = Account.builder().accountType(AccountType.SAVINGS)
+        .customer(employeeUser).balance(1000.0).isActive(true).
+        build();
+    accountRepository.save(savingsAccount);
+    List<Account> foundAccounts = accountRepository.findAll(
+        AccountSpecifications.isCurrentAccounts());
+    assertEquals(1, foundAccounts.size());
+    assertTrue(foundAccounts.contains(employeeAccount));
+  }
 
-    @Override
-    public List<Account> findAll(Specification<Account> spec) {
-      return null;
-    }
 
-    @Override
-    public Page<Account> findAll(Specification<Account> spec, Pageable pageable) {
-      return null;
-    }
-
-    @Override
-    public List<Account> findAll(Specification<Account> spec, Sort sort) {
-      return null;
-    }
-
-    @Override
-    public long count(Specification<Account> spec) {
-      return 0;
-    }
-
-    @Override
-    public boolean exists(Specification<Account> spec) {
-      return false;
-    }
-
-    @Override
-    public long delete(Specification<Account> spec) {
-      return 0;
-    }
-
-    @Override
-    public <S extends Account, R> R findBy(Specification<Account> spec,
-        Function<FluentQuery.FetchableFluentQuery<S>, R> queryFunction) {
-      return null;
-    }
-
-    @Override
-    public void flush() {
-
-    }
-
-    @Override
-    public <S extends Account> S saveAndFlush(S entity) {
-      return null;
-    }
-
-    @Override
-    public <S extends Account> List<S> saveAllAndFlush(Iterable<S> entities) {
-      return null;
-    }
-
-    @Override
-    public void deleteAllInBatch(Iterable<Account> entities) {
-
-    }
-
-    @Override
-    public void deleteAllByIdInBatch(Iterable<String> strings) {
-
-    }
-
-    @Override
-    public void deleteAllInBatch() {
-
-    }
-
-    @Override
-    public Account getOne(String s) {
-      return null;
-    }
-
-    @Override
-    public Account getById(String s) {
-      return null;
-    }
-
-    @Override
-    public Account getReferenceById(String s) {
-      return null;
-    }
-
-    @Override
-    public <S extends Account> Optional<S> findOne(Example<S> example) {
-      return Optional.empty();
-    }
-
-    @Override
-    public <S extends Account> List<S> findAll(Example<S> example) {
-      return null;
-    }
-
-    @Override
-    public <S extends Account> List<S> findAll(Example<S> example, Sort sort) {
-      return null;
-    }
-
-    @Override
-    public <S extends Account> Page<S> findAll(Example<S> example, Pageable pageable) {
-      return null;
-    }
-
-    @Override
-    public <S extends Account> long count(Example<S> example) {
-      return 0;
-    }
-
-    @Override
-    public <S extends Account> boolean exists(Example<S> example) {
-      return false;
-    }
-
-    @Override
-    public <S extends Account, R> R findBy(Example<S> example,
-        Function<FluentQuery.FetchableFluentQuery<S>, R> queryFunction) {
-      return null;
-    }
-
-    @Override
-    public Page<Account> findAccountByAccountTypeEqualsAndIbanNot(Pageable pageable,
-        AccountType accountType, String iban) {
-      return null;
-    }
-
-    @Override
-    public Page<Account> findByAndIbanNot(Pageable pageable, String iban) {
-      return null;
-    }
-
-    @Override
-    public List<Account> findByCustomer_IdEquals(long id) {
-      return null;
-    }
-
-    @Override
-    public int countAccountByCustomer_IdEqualsAndAccountTypeEquals(long customerId,
-        AccountType accountType) {
-      return 0;
-    }
-
-    @Override
-    public boolean existsAccountByIbanEqualsAndCustomerEmailEqualsIgnoreCase(String iban,
-        String email) {
-      return false;
-    }
-  };
 }
--- src/test/java/com/term4/BankingAppGrp1/repositories/UserRepositoryTest.java
+++ src/test/java/com/term4/BankingAppGrp1/repositories/UserRepositoryTest.java
@@ -1,21 +1,68 @@
 package com.term4.BankingAppGrp1.repositories;
 
-import com.term4.BankingAppGrp1.configuration.ApiTestConfiguration;
-import com.term4.BankingAppGrp1.testingData.BankingAppTestData;
-import org.junit.jupiter.api.BeforeEach;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.context.annotation.Import;
-import org.springframework.test.annotation.DirtiesContext;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@DataJpaTest
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
-//making issolated for each tests
-@Import(ApiTestConfiguration.class)
-public class UserRepositoryTest extends BankingAppTestData {
+import com.term4.BankingAppGrp1.models.User;
+import java.util.Optional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+class UserRepositoryTest extends BaseRepositoryTest {
 
   @BeforeEach
   protected void setupData() {
     super.setupData();
   }
+
+  @Test
+  void findByEmailShouldReturnAValidUserIfContainsInDb() {
+    User savedUser = userRepository.save(customerUser);
+    Optional<User> findingUser = userRepository.findByEmail(customerUser.getEmail());
+    assert (findingUser.isPresent());
+    assert (findingUser.get().getEmail().equals(customerUser.getEmail()));
+    assertEquals(savedUser, findingUser.get());
+  }
+
+  @Test
+  void findByEmailShouldReturnEmptyIfNotContainsInDb() {
+    Optional<User> findingUser = userRepository.findByEmail(customerUser.getEmail());
+    assertTrue(findingUser.isEmpty());
+  }
+
+  @Test
+  void findByEmailEqualsIgnoreCaseShouldReturnAValidUserIfContainsInDb() {
+    User savedUser = userRepository.save(customerUser);
+    Optional<User> findingUser = userRepository.findByEmailEqualsIgnoreCase(
+        customerUser.getEmail().toUpperCase());
+    assert (findingUser.isPresent());
+    assert (findingUser.get().getEmail().equals(customerUser.getEmail()));
+    assertEquals(savedUser, findingUser.get());
+  }
+
+  @Test
+  void saveUserShouldSaveTheUserInDb() {
+    User savedUser = userRepository.save(customerUser);
+    Optional<User> findingUser = userRepository.findById(savedUser.getId());
+    assert (findingUser.isPresent());
+    assertEquals(savedUser, findingUser.get());
+  }
+
+  @Test
+  void findByBsnShouldReturnAValidUserIfContainsInDb() {
+    User savedUser = userRepository.save(customerUser);
+    Optional<User> findingUser = userRepository.findByBsn(customerUser.getBsn());
+    assert (findingUser.isPresent());
+    assert (findingUser.get().getBsn().equals(customerUser.getBsn()));
+    assertEquals(savedUser, findingUser.get());
+  }
+
+  @Test
+  void findByBsnShouldReturnEmptyIfNotContainsInDb() {
+    Optional<User> findingUser = userRepository.findByBsn(customerUser.getBsn());
+    assertTrue(findingUser.isEmpty());
+  }
+
 
 }
